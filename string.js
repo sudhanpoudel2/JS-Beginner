9e790ceb16bf95a6670ca0306d4951caea76a180
Sudhan Poudel <120155625+sudhanpoudel2@users.noreply.github.com>
2023-10-27 08:36:25 +0545

    done

---
+++ string.js
@@ -0,0 +1,13 @@
+
+const name = "Sudhan";
+const repoCount = 19;
+
+// console.log(name + repoCount + " Value");
+
+console.log(`My name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("MMA Fighter");
+
+console.log(gameName[0]);
+console.log(gameName.toUpperCase);
+console.log(gameName.length);
