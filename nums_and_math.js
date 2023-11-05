53bc39497ae4b058d3e5ad7b26b1fe4e64afbb75
Sudhan Poudel <120155625+sudhanpoudel2@users.noreply.github.com>
2023-11-05 12:46:29 +0545

    done

--- nums_and_math.js
+++ nums_and_math.js
@@ -3,28 +3,28 @@
  const score = 400;
 const balance = new Number (100);
 
-//  console.log(balance);
-//  console.log(score);
+ console.log(balance);
+ console.log(score);
 
-//  console.log(balance.toString().length);
-//  console.log(balance.toFixed(1));
+ console.log(balance.toString().length);
+  console.log(balance.toFixed(1));
 
  const otherNumber = 23.4458;
 
-//  console.log(otherNumber.toPrecision(3));
+ console.log(otherNumber.toPrecision(3));
 
  const balanceNumber = 1000000;
 //  console.log(balanceNumber.toLocaleString('en-IN'));
 
  /*****************************MATH***********************************/
 
-//  console.log(Math);
-//  console.log(Math.abs(-4));
-//  console.log(Math.round(4.7));
-//  console.log(Math.ceil(4.2));
-//  console.log(Math.floor(4.9));
-//  console.log(Math.min(2, 5, 8, 1));
-//  console.log(Math.max(2, 4, 8));
+ console.log(Math);
+ console.log(Math.abs(-4));
+ console.log(Math.round(4.7));
+ console.log(Math.ceil(4.2));
+ console.log(Math.floor(4.9));
+ console.log(Math.min(2, 5, 8, 1));
+ console.log(Math.max(2, 4, 8));
 
 console.log(Math.random());
 console.log((Math.random()*10)+1);
