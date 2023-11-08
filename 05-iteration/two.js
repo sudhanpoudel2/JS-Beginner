5b97183560627a02451a10e371700fcfdcbf85df
Sudhan Poudel <120155625+sudhanpoudel2@users.noreply.github.com>
2023-11-08 18:20:46 +0545

    done

---
+++ 05-iteration/two.js
@@ -0,0 +1,20 @@
+
+let i = 0;
+while (i<=10) {
+    console.log(`Value of index is ${i}`);
+    i = i+2;
+}
+
+let heros= ["flash" , "batman" , "superman"];
+let arr = 0;
+while (arr<heros.length) {
+    console.log(`Value is ${heros[arr]}`);
+    arr = arr+1;
+}
+
+let score = 1;
+
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score<=20);
